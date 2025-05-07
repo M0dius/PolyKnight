af753b61fcfb47f27d7c8bc9006bb6590ab1977c
mmahmood233 <m7mdm233@gmail.com>
2025-05-07 15:25:02 +0300

    cleaned and no errors

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,35 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+//public class Player : MonoBehaviour
+//{
 
-    public int maxHealth = 100;
-    public int currentHealth;
+//    public int maxHealth = 100;
+//    public int currentHealth;
 
-    public Health healthBar;
+//    public Health healthBar;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
-    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        currentHealth = maxHealth;
+//        healthBar.SetMaxHealth(maxHealth);
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(10);
-        }
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.Space))
+//        {
+//            TakeDamage(10);
+//        }
+//    }
 
-    void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
+//    void TakeDamage(int damage)
+//    {
+//        currentHealth -= damage;
 
-        healthBar.SetHealth(currentHealth);
-    }
-}
+//        healthBar.SetHealth(currentHealth);
+//    }
+//}
